ab53b0e9c4cc84335fbb5a855260e35e602bf050
joeowen22 <joseph_owen@ymail.com>
2018-02-01 20:53:16 +0000

    Added some more comments

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
+# Copy the NodeJS carbon image
 FROM node:carbon
-# App directory
+# Directory where the app will run from
 WORKDIR /usr/src/app
 # This is required so that the application dependencies can be installed
 COPY package*.json ./
 RUN npm install
-# Some node files have been included in the .dockerignore fil
+# Some node files have been included in the .dockerignore file that do not need adding
+# Copy over the app files
 COPY . .
+# Expose port 8080 in the container
 EXPOSE 8080
+# Start the nodejs server
 CMD ["npm","start"]
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict';
-
+/**
+ * This is based on the example from
+ * https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
+ */
 const express = require('express');
 
 
-// Constants
+// Variables for running
+// Need to look why host 0.0.0.0 is use, I am unsure why?
 const HOST = '0.0.0.0';
 const PORT = 8080;
 
@@ -16,21 +20,27 @@ let voteObjects = {
     ]
 };
 
+// Create an object that suits a response
 let responseObject = {
     status: 'SUCCESS',
     data: voteObjects
 };
 
+
+// Initialise the app using express
 const app = express();
+
+// Dummy hello world return to check server is running
 app.get('/', (req,res) => {
-    res.send('Hello World');
+    res.send('Hello World\n');
 });
 
+// Returns a JSON object to mock a ws response
 app.get('/getVotes', (req, res) => {
-    res.header("Content-type", "application/json");
-    res.send(JSON.stringify(responseObject));
+    res.header('Content-type', 'application/json');
+    res.send(JSON.stringify(responseObject)+'\n');
 });
 
-
+// Tell server to start listening for requests on PORT and HOST
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
